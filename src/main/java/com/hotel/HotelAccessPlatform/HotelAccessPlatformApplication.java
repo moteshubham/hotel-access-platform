37ca1bf4e83cb36420996aca3433ff1a6431f95f
moteshubham <moteshubham@gmail.com>
2025-12-06 12:06:24 +0530

    initial commit

---
+++ src/main/java/com/hotel/HotelAccessPlatform/HotelAccessPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.HotelAccessPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAccessPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAccessPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/HotelAccessPlatform/HotelAccessPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.HotelAccessPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAccessPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
